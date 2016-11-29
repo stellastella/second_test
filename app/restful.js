baee8be816767b52d84d377ca5c2490e0ffb72a1
stellastella <wuchukwustella333@gmail.com>
2016-11-29 18:34:44 +0100

    initial commit

--- app/restful.js
+++ app/restful.js
@@ -1,7 +1,18 @@
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+
+
+function getquote(){
+var http = require('http')
+ var options = {
+        host: 'https:/'/andruxnet-random-famous-quotes.p.mashape.com/'cat=',
+        method: 'GET',
+        headers: {'Quotes': 'node.js'}
+    };
+http.get(options,function(response){
+	response.on('data', console.log)
+
+response.on('error', console.error)
+
+response.setEncoding('utf8')} 
+)};
+    
+    getquote();
